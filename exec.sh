fe321c4969bf5b9d4e88304d712e9f9c41a99049
tryuhowcollect <t.ryu.howcollect@gmail.com>
2024-04-15 14:15:16 +0900

    ビルド並列処理

--- exec.sh
+++ exec.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 eval $(minikube docker-env)
-# v1バージョンのDockerイメージをビルド
-docker build -f Dockerfile.v1 -t weather-app:v1 .
-# v2バージョンのDockerイメージをビルド
-docker build -f Dockerfile.v2 -t weather-app:v2 .
+
+# v1バージョンとv2バージョンのDockerイメージを並列でビルド
+docker build -f Dockerfile.v1 -t weather-app:v1 . &
+docker build -f Dockerfile.v2 -t weather-app:v2 . &
+
+# すべてのバックグラウンドプロセスが終了するのを待つ
+wait
+
 kubectl apply -f k8s/deployment.yml
 kubectl apply -f k8s/service.yml
+
+# Podの状態を表示
 kubectl get pods
+
+# MinikubeのサービスURLを取得
 minikube service weather-service --url
